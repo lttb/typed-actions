6ddeb1d49c3d81e08e43654be8eb9ebe0754c54b
antonk52 <halloy52@gmail.com>
2021-02-10 16:27:20 +0300

    typescript

---
+++ dts/immer.d.ts
@@ -0,0 +1,17 @@
+import type {Reducer} from 'redux-actions';
+
+// utils
+type AbstractActions = Record<string, (...args: any) => any>;
+type $Exact<T> = T & {[key in string]?: T[keyof T] | never};
+export type ExtractActionsFromHandlers<H extends Handlers<any, AbstractActions>> = {
+    [K in keyof H]: Required<H>[K] extends (...args: [any, infer Action, ...any[]]) => any ? Action : never;
+}[keyof H];
+export type ExtractStateFromHandlers<H extends Handlers<any, AbstractActions>> = {
+    [K in keyof H]: Required<H>[K] extends (...args: [infer State, ...any[]]) => any ? State : never;
+}[keyof H];
+
+export type Handlers<S extends unknown, A extends AbstractActions> = {
+    [K in keyof A]?: (state: S, action: ReturnType<A[K]>) => void;
+}
+
+export declare function handleActions<T extends Handlers<any, AbstractActions>>(arg: T): Reducer<ExtractStateFromHandlers<T>, ExtractActionsFromHandlers<T>>
